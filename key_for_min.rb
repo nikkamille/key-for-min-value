6c0866bffb26527553fba2922f2556017d2a5076
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-10 22:23:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,6 @@ def key_for_min_value(name_hash)
   smallest_key = nil 
   smallest_value = nil 
   name_hash.each do |name, number|
-    if smallest_value == nil ||  
+    if smallest_value == nil || number < smallest_value 
   end
 end
